95e40674f35b4bd839dd2a8fca9afb517e2a71ba
Carlos Sanjuán Comino <carlossc604@gmail.com>
2025-02-04 21:49:40 +0100

    Updated pdf generation

--- src/util/DocGenerator.ts
+++ src/util/DocGenerator.ts
@@ -3,44 +3,8 @@ import fs from "fs";
 
 export async function generateInvoice(filename: string) {
     const pdf = await PDF.create(filename);
-    pdf.writeLine("Ejemplo S.A.");
-    pdf.writeLine("Ejemplo S.A.kjfbsdfjkfgbskhfbdshfbasdhfsadjfbdfjbfjhsavjsfvasiyhfgasksjbfkhsdbvciysabfhsbfvyisdvfhisavbfhsadfvhjasvfhasbvfkhasfvbkhldsbfvadhsfbvhklasdfvadsvfkadhsvfadsvfjhasvfasvdfhjdsavfasdvfhjvfayisfdvyasdgfvfbasdjfvasdfghvasfbvsafj");
+   
 
-    pdf.drawTable({
-        borderColor: rgb(0, 0, 0),
-        borderWidth: 0.6,
-        headers: ["Item", "Description", "Price", "Quantity"],
-        fields: [
-            ["Item 1", "Description 1", "10", "1"],
-            ["Item 2", "Description 2", "20", "2"],
-            ["Item 3", "Description 3", "30", "3"],
-        ],
-        cellPadding: 4
-    });
-
-    pdf.drawTable({
-        borderColor: rgb(0, 0, 0),
-        borderWidth: 0.6,
-        headers: ["Item", "Description", "Price", "Quantity"],
-        fields: [
-            ["Item 1", "Description 1", "10", "1"],
-            ["Item 2", "Description 2", "20", "2"],
-            ["Item 3", "Description 3", "30", "3"],
-        ],
-        cellPadding: 4
-    });
-
-    pdf.drawTable({
-        borderColor: rgb(0, 0, 0),
-        borderWidth: 0.6,
-        headers: ["Item", "Description", "Price", "Quantity"],
-        fields: [
-            ["Item 1", "Description 1", "10", "1"],
-            ["Item 2", "Description 2", "20", "2"],
-            ["Item 3", "Description 3", "30", "3"],
-        ],
-        cellPadding: 4
-    });
     pdf.save();
 }
 
@@ -94,60 +58,128 @@ class PDF{
 
         const textFont = options.font ? options.font : this.pdfDoc.embedStandardFont(StandardFonts.Helvetica);
 
-        const lines = this.countWrappedLines(text, textFont, options.size, options.maxWidth);
-        this.page.drawText(text, {...options});
+        const printedHeight = this.printText(text, textFont, options.size, options.x, options.y, options.maxWidth);
 
-        this.y -= (options.lineHeight * lines);
+        this.y -= printedHeight;
     }
 
     drawTable(options: TableOptions<string[]>) {
-        const cellTotalHeight = 10 + options.cellPadding*2;
-        this.y -= (this.lineHeight + cellTotalHeight);
         const font = this.pdfDoc.embedStandardFont(StandardFonts.Helvetica);
-        const columns = options.headers.length;
-        const columnWidth = (this.width- (this.padding * 2)) / columns;
+        const columnWidth = (this.width - this.padding*2)/(options.headers.length);
+        const usableColumnWidth = columnWidth - (options.cellPadding * 2);
+        const higherHeaderRow = Math.max(...options.headers.map(text => this.getHeightWhenPrinted(text, font, 10, usableColumnWidth)));
 
-        for (let i = 0; i < columns; i++) {
+        //Print The headers
+        for (let i = 0; i < options.headers.length; i++) {
+            const headerCellHeight = this.getHeightWhenPrinted(options.headers[i], font, 10, usableColumnWidth);
+            this.printText(
+                options.headers[i], 
+                font, 10, 
+                this.x + options.cellPadding + (i*columnWidth), 
+                this.y - options.cellPadding - (higherHeaderRow - headerCellHeight)/2 - this.lineHeight/2, 
+                usableColumnWidth,
+                "center"
+            );
             this.page.drawRectangle({
                 borderColor: options.borderColor,
-                borderWidth: options.borderWidth,
-                x: this.x +(columnWidth * i),
-                y: this.y,
-                width: columnWidth ,
-                height: cellTotalHeight,
+                height: higherHeaderRow + options.cellPadding*2,
+                borderWidth:  options.borderWidth,
+                width: columnWidth,
+                x: this.x + (i*columnWidth),
+                y: this.y - (higherHeaderRow) - options.cellPadding-(this.lineHeight -10)/2
             });
-            const [textHeight, textWidth] = this.getTextHeightWidth(options.headers[i], font, 10);
-            
-            this.page.drawText(options.headers[i], {
-                size: 10,
-                x: this.x+(columnWidth*i) + ((columnWidth - textWidth)/2), 
-                y: this.y+options.cellPadding+(this.lineHeight - textHeight)/2,
-            })
         }
-        this.y -= (cellTotalHeight);
+        this.y -= higherHeaderRow+options.cellPadding*2;
 
         for (let i = 0; i < options.fields.length; i++) {
+            const higherFieldRow = Math.max(...options.fields[i].map(text => this.getHeightWhenPrinted(text, font, 10, usableColumnWidth)));
             for (let j = 0; j < options.fields[i].length; j++) {
+                const fieldCellHeight = this.getHeightWhenPrinted(options.fields[i][j], font, 10, usableColumnWidth);
+                this.printText(
+                    options.fields[i][j], 
+                    font, 10, 
+                    this.x + options.cellPadding + (j*columnWidth), 
+                    this.y - options.cellPadding - (higherFieldRow - fieldCellHeight)/2 - this.lineHeight/2, 
+                    usableColumnWidth,
+                    "center"
+                );
                 this.page.drawRectangle({
                     borderColor: options.borderColor,
-                    borderWidth: options.borderWidth,
-                    x: this.x +(columnWidth * j),
-                    y: this.y,
-                    width: columnWidth ,
-                    height: cellTotalHeight,
+                    height: higherFieldRow + options.cellPadding*2,
+                    borderWidth:  options.borderWidth,
+                    width: columnWidth,
+                    x: this.x + (j*columnWidth),
+                    y: this.y - (higherFieldRow) - options.cellPadding-(this.lineHeight -10)/2
                 });
-
-                const [textHeight, textWidth] = this.getTextHeightWidth(options.fields[i][j], font, 10);
-
-                this.page.drawText(options.fields[i][j], {
-                    size: 10,
-                    x: this.x+(columnWidth*j) + ((columnWidth - textWidth)/2), 
-                    y: this.y+options.cellPadding+(this.lineHeight - textHeight)/2,
-                })
             }
-            this.y -= (cellTotalHeight);
+            this.y -= higherFieldRow+options.cellPadding*2;
         }
 
+    }
+
+    private wrapText(text: string, font: PDFFont, fontSize: number, maxWidth: number) {
+        const lines: string[] = [];
+        const words = text.split(" ");
+        let currentLine = "";
+
+        for (const word of words) {
+            if(font.widthOfTextAtSize(`${currentLine} ${word}`, fontSize) <= maxWidth){
+                currentLine = [currentLine, word].join(' ').trim();
+            }else if(font.widthOfTextAtSize(word, fontSize) > maxWidth){                
+                currentLine += ' ';
+                for (const char of word) {
+                    const testLine = currentLine + char;
+                    const textWidth = font.widthOfTextAtSize(testLine, fontSize);
+            
+                    if (textWidth > maxWidth && currentLine.length > 0) {
+                        lines.push(currentLine);
+                        currentLine = char;
+                    } else {
+                        currentLine = testLine;
+                    }
+                }
+            }else{
+                lines.push(currentLine);
+                currentLine = word;
+            }
+        }
+        
+        if (currentLine.trim() != '') lines.push(currentLine);
+        
+        return lines;
+    }
+
+    private getHeightWhenPrinted(text: string, font: PDFFont, fontSize: number, maxWidth: number){
+        const lines = this.wrapText(text, font, fontSize, maxWidth);
+        return lines.length * this.lineHeight;
+    }
+
+    private printText(text: string, font: PDFFont, fontSize: number, x: number, y: number, maxWidth: number, alignment: "left"|"center"|"right" = "left") {
+        const lines = this.wrapText(text, font, fontSize, maxWidth);
+        let plusX = 0;
+        
+
+        for (let i = 0; i < lines.length; i++) {
+            if((this.y - this.lineHeight) < this.padding) {
+                this.addPage();
+            }
+            if(alignment === "left"){
+                plusX = 0;
+            }else if(alignment === "center"){
+                plusX = (maxWidth - this.getTextWidth(lines[i], font, fontSize))/2;
+            }else if(alignment === "right"){
+                plusX = maxWidth - this.getTextWidth(lines[i], font, fontSize);
+            }
+            this.page.drawText(lines[i], {
+                size: fontSize,
+                x: x + plusX,
+                y: y - (i * this.lineHeight),
+                maxWidth: maxWidth,
+                lineHeight: this.lineHeight,
+            })
+        }
+
+        return lines.length * this.lineHeight;
     }
 
     //Used just for counting the number of lines wrapped when writing a line
@@ -172,10 +204,9 @@ class PDF{
         return lines;
     }
 
-    private getTextHeightWidth(text: string, font: PDFFont, fontSize: number) {
-        const textHeight = font.heightAtSize(fontSize);
+    private getTextWidth(text: string, font: PDFFont, fontSize: number) {
         const textWidth = font.widthOfTextAtSize(text, fontSize);
-        return [textHeight, textWidth];
+        return textWidth;
     }
 
     async save(){
